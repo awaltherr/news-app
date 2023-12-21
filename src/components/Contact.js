3b0615950c18a80030b2ee3f80dbcfed056e11d2
awaltherr <anton.walther@live.com>
2023-12-21 21:11:46 +0100

    Applied layout for contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,27 @@
-import { ContactContainer } from "../styles/Contact.styled";
+import {
+  ContactContainer,
+  InputGroup,
+  InputField,
+  TextArea,
+  ContactSubmitBtn,
+} from "../styles/Contact.styled";
 
 export default function Contact() {
   return (
     <ContactContainer>
-      <h1>Contact</h1>
+      <InputGroup>
+        <InputField type="text" id="name" placeholder="Name" required />
+      </InputGroup>
+      <InputGroup>
+        <InputField type="text" id="number" placeholder="Phone" required />
+      </InputGroup>
+      <InputGroup>
+        <InputField type="text" id="email" placeholder="Email" required />
+      </InputGroup>
+      <InputGroup>
+        <TextArea id="message" rows="12" placeholder="Message" />
+      </InputGroup>
+      <ContactSubmitBtn type="submit">Submit</ContactSubmitBtn>
     </ContactContainer>
   );
 }
--- src/styles/Contact.styled.js
+++ src/styles/Contact.styled.js
@@ -1,8 +1,42 @@
 import styled from "styled-components";
 
-export const ContactContainer = styled.div`
+const inputStyle = `
+  width: 100%;
+  padding: 10px;
+  border: 1px solid white;
+  outline: none;
+  background: white;
+  font-size: 15px;
+`;
+
+export const ContactContainer = styled.form`
+  padding-top: 200px;
+  width: 90%;
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+export const InputGroup = styled.div`
+  margin-bottom: 30px;
+`;
+
+export const InputField = styled.input`
+  ${inputStyle}
+`;
+
+export const TextArea = styled.textarea`
+  ${inputStyle}
+`;
+
+export const ContactSubmitBtn = styled.button`
   display: flex;
+  align-items: flex-start;
   justify-content: center;
-  align-items: center;
-  height: 100vh;
+  padding: 10px;
+  outline: none;
+  width: 40%;
+  cursor: pointer;
+  background-color: #c3e2c2;
+  color: black;
+  border: none;
 `;
