19ec96723ccc2ad22288652a1b1eb00b2fd48d8e
awaltherr <anton.walther@live.com>
2023-12-25 19:07:53 +0100

    Added validation to contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import {
   ContactContainer,
   InputGroup,
@@ -7,21 +8,125 @@ import {
 } from "../styles/Contact.styled";
 
 export default function Contact() {
+  const [contactInputs, setContactInputs] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    message: "",
+  });
+  const [contactErrors, setContactErrors] = useState({});
+  const [contactSubmit, setContactSubmit] = useState(false);
+  const [successSubmit, setSuccessSubmit] = useState(false);
+
+  const validateForm = (inputValues) => {
+    let errors = {};
+
+    if (!inputValues.firstname.trim()) {
+      errors.firstname = "First name cannot be empty";
+    }
+
+    if (!inputValues.lastname.trim()) {
+      errors.lastname = "Last name cannot be empty";
+    }
+
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    if (!inputValues.email.trim()) {
+      errors.email = "Email cannot be empty";
+    } else if (!emailRegex.test(inputValues.email)) {
+      errors.email = "Please use valid email";
+    }
+
+    if (!inputValues.message.trim()) {
+      errors.message = "Message cannot be empty";
+    } else if (inputValues.message.length < 10) {
+      errors.email = "Message must contain at least 15 characters";
+    }
+
+    return errors;
+  };
+
+  const handleChange = (e) => {
+    setContactInputs({ ...contactInputs, [e.target.name]: e.target.value });
+  };
+
+  const clearFields = () => {
+    setContactInputs({
+      firstname: "",
+      lastname: "",
+      email: "",
+      message: "",
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const errors = validateForm(contactInputs);
+    setContactErrors(errors);
+    if (Object.keys(errors).length === 0) {
+      setContactSubmit(true);
+    }
+  };
+
+  useEffect(() => {
+    const finishSubmit = () => {
+      console.log(contactInputs);
+      setSuccessSubmit(true);
+      clearFields();
+    };
+    if (contactSubmit) {
+      finishSubmit();
+    }
+  }, [contactSubmit, contactInputs]);
+
   return (
-    <ContactContainer>
+    <ContactContainer onSubmit={handleSubmit}>
       <InputGroup>
-        <InputField type="text" id="name" placeholder="Name" required />
+        <InputField
+          type="text"
+          name="firstname"
+          placeholder="First name*"
+          onChange={handleChange}
+          value={contactInputs.firstname}
+        />
       </InputGroup>
       <InputGroup>
-        <InputField type="text" id="number" placeholder="Phone" required />
+        <InputField
+          type="text"
+          name="lastname"
+          placeholder="Last name*"
+          onChange={handleChange}
+          value={contactInputs.lastname}
+        />
       </InputGroup>
       <InputGroup>
-        <InputField type="text" id="email" placeholder="Email" required />
+        <InputField
+          type="text"
+          name="email"
+          placeholder="Email*"
+          onChange={handleChange}
+          value={contactInputs.email}
+        />
       </InputGroup>
       <InputGroup>
-        <TextArea id="message" rows="12" placeholder="Message" />
+        <TextArea
+          name="message"
+          rows="12"
+          placeholder="Message*"
+          onChange={handleChange}
+          value={contactInputs.message}
+        />
       </InputGroup>
       <ContactSubmitBtn type="submit">Submit</ContactSubmitBtn>
+      {Object.keys(contactErrors).length > 0 && (
+        <ul className="errorList">
+          {Object.values(contactErrors).map((error, index) => (
+            <li key={index}>{error}</li>
+          ))}
+        </ul>
+      )}
+      {successSubmit && (
+        <span className="successSubmit">Thank you for your message</span>
+      )}
     </ContactContainer>
   );
 }
